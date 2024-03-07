1cf27bcd5a478c4c8133c57853741414e8ded05a
Lesia <157366599+P4ela5@users.noreply.github.com>
2024-03-07 13:45:26 +0200

    54

--- js/task_3.js
+++ js/task_3.js
@@ -1,9 +1,9 @@
 function getElementWidth(content, padding, border) {
-const contentSize = parseFloat(content);
-const paddingSize = parseFloat(padding);
-const borderSize = parseFloat(border);
-const sizeWidth = contentSize + 2 * paddingSize + 2 * borderSize;
-return(sizeWidth);
+    const contentSize = parseFloat(content);
+    const paddingSize = parseFloat(padding);
+    const borderSize = parseFloat(border);
+    const sizeWidth = contentSize + 2 * paddingSize + 2 * borderSize;
+    return(sizeWidth);
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
